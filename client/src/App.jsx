ceb2d571ec4ecbf908773fef3bee975da13ac827
cuong07 <front.cuong@gmail.com>
2024-05-08 15:20:43 +0700

    feat: init front end react, react-router, antd

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      App
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default App;
