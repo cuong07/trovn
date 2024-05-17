bd4848911927e3bf2ba93fbfa6e6144074a73cc8
TheCong234 <127668626+TheCong234@users.noreply.github.com>
2024-05-17 09:25:26 +0700

    handle login submit

--- client/src/apis/user.js
+++ client/src/apis/user.js
@@ -9,3 +9,12 @@ export const register = async (data) => {
     const user = await apiClient.post(url, data);
     return user.data;
 }
+
+export const login = async (data) =>{
+    const url = qs.stringifyUrl({
+        url: UserV1.USER_LOGIN,
+    })
+    // await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+    const user = await apiClient.post(url, data);
+    return user.data;
+}
