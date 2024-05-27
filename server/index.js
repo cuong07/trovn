bafbfde6f09262e90ed2db79392169f4f1b9a74d
cuong07 <front.cuong@gmail.com>
2024-05-27 14:29:57 +0700

    update

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
--- server/socket/index.js
+++ server/socket/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   },
 });
