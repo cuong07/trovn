bd390cfc098dd3536f0979a0a79f1b39c957f2e0
cuong07 <front.cuong@gmail.com>
2024-05-27 14:26:14 +0700

    update

--- server/index.js
+++ server/index.js
@@ -24,8 +24,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "*",
-    // credentials: true,
+    origin: "https://localhost:5173",
+    credentials: true,
   })
 );
 
