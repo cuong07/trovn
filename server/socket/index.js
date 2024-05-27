35a6fb61aad2672ecf219e329614beff9d0c6bfe
cuong07 <front.cuong@gmail.com>
2024-05-27 14:37:58 +0700

    update

--- server/socket/index.js
+++ server/socket/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://127.0.0.1:5173",
     credentials: true,
   },
 });
