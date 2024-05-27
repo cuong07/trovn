3c149aa07df4795c67a809635a2087d96a8091da
cuong07 <front.cuong@gmail.com>
2024-05-27 13:23:16 +0700

    udpate env

--- client/src/apis/apiClient.js
+++ client/src/apis/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import useUserStore from "../hooks/userStore";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8888/api/v1",
+  baseURL: import.meta.env.VITE_APP_BACKEND_URL + "/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
