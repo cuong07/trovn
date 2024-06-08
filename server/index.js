9d899b3951bf812d34c6a22a2900162473ad4dc3
cuong07 <front.cuong@gmail.com>
2024-06-08 11:12:11 +0700

    rate limit

--- server/index.js
+++ server/index.js
@@ -4,21 +4,22 @@ import bodyParser from "body-parser";
 import cron from "node-cron";
 import BannerModel from "./models/banner.model.js";
 import { app, server } from "./socket/index.js";
+import { rateLimit } from "express-rate-limit";
 import {
-    UserRoutes,
-    AmenityRoutes,
-    ListingRoutes,
-    LocationRoutes,
-    FavoriteRoutes,
-    BannerRoutes,
-    TagRoutes,
-    ListingTagRoutes,
-    AdvertisingPackageRoutes,
-    PaymentRoutes,
-    OrderRoutes,
-    ConversationRoutes,
-    AnalyticsRoutes,
-    GoogleAuthRoutes,
+  UserRoutes,
+  AmenityRoutes,
+  ListingRoutes,
+  LocationRoutes,
+  FavoriteRoutes,
+  BannerRoutes,
+  TagRoutes,
+  ListingTagRoutes,
+  AdvertisingPackageRoutes,
+  PaymentRoutes,
+  OrderRoutes,
+  ConversationRoutes,
+  AnalyticsRoutes,
+  GoogleAuthRoutes,
 } from "./routes/index.js";
 import "./config/passport.config.js";
 import session from "express-session";
@@ -31,17 +32,28 @@ app.use(express.json({ limit: "30mb" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-    cors({
-        origin: "*",
-        // credentials: true,
-    })
+  cors({
+    origin: "*",
+    // credentials: true,
+  })
 );
 app.use(
-    session({
-        secret: "your-secret-key",
-        resave: false,
-        saveUninitialized: true,
-    })
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// TODO: Rate limit
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 150,
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  })
 );
 
 // setuppassport
@@ -69,16 +81,16 @@ app.use(express.static("./public"));
 
 // TODO: run update banner 00h00
 cron.schedule("0 0 * * *", async () => {
-    console.log(
-        "Chạy công việc theo lịch trình để cập nhật các banner đã hết hạn."
-    );
-    await BannerModel.methods.updateExpiredBanners();
+  console.log(
+    "Chạy công việc theo lịch trình để cập nhật các banner đã hết hạn."
+  );
+  await BannerModel.methods.updateExpiredBanners();
 });
 
 app.get("*", (req, res) => {
-    res.status(404).send("Sorry, resource not found");
+  res.status(404).send("Sorry, resource not found");
 });
 
 server.listen(PORT, () => {
-    console.log(`Server running on port: http://localhost:${PORT}`);
+  console.log(`Server running on port: http://localhost:${PORT}`);
 });
