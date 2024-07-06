8f4e076646272006202279b33ad6c33f6f288747
cuong07 <front.cuong@gmail.com>
2024-07-07 00:11:50 +0700

    feat: generate desc by gemini ai

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+const { default: axios } = require("axios");
+const { useState, useEffect } = require("react");
+
+const useFetch = (url) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [apiData, setApiData] = useState(null);
+    const [serverError, setServerError] = useState(null);
+
+    useEffect(() => {
+        setIsLoading(true);
+        (async () => {
+            try {
+                const res = await axios.get(url);
+                const data = await res?.data;
+
+                setApiData(data);
+                setIsLoading(false);
+            } catch (error) {
+                setIsLoading(false);
+                setServerError(error);
+            }
+        })();
+    }, [url]);
+    return { isLoading, apiData, serverError };
+};
+
+export default useFetch;
